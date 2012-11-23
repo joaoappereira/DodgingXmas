028d550d382422153d4a3ecd07b7ccebc960f45c
jappereira <jappereira@gmail.com>
2012-11-23 14:00:36 +0000

    Changed sensors

--- src/pt/snowplow/dodgingxmas/MainActivity.java
+++ src/pt/snowplow/dodgingxmas/MainActivity.java
@@ -1,20 +1,24 @@
 package pt.snowplow.dodgingxmas;
 
+import android.R;
 import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
 	private MainView mainView;
 
 	private SensorManager sensorManager;
-	private Sensor sensorRotationVector;
-	private final float[] rotationMatrix = new float[16];
-	private final int SPEED = 2;
+	private Sensor sensorAccelerometer;
+	private Sensor sensorMagneticField;
+	private float[] tiltData = {0, 0, 0};
+	private float[] gravityData = {0, 0, 0};
+	private float[] magneticData = {0, 0, 0};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +26,14 @@ public class MainActivity extends Activity implements SensorEventListener {
 		setContentView(R.layout.activity_main);
 		mainView = (MainView) findViewById(R.id.main_view);
 		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-		sensorRotationVector = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-		initializeRotationMatrix();
+		sensorAccelerometer = sensorManager.getDefaultSensor( Sensor.TYPE_ACCELEROMETER );
+		sensorMagneticField = sensorManager.getDefaultSensor( Sensor.TYPE_MAGNETIC_FIELD );
 		enableSensor();
 	}
 
-	// SENSOR
-	private void initializeRotationMatrix() {
-		rotationMatrix[0] = 1;
-		rotationMatrix[1] = 1;
-		rotationMatrix[2] = 1;
-		rotationMatrix[3] = 1;
-	}
-
 	private void enableSensor() {
-		sensorManager.registerListener(this, sensorRotationVector, SensorManager.SENSOR_DELAY_GAME);
+		sensorManager.registerListener(this, sensorAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+		sensorManager.registerListener(this, sensorMagneticField, SensorManager.SENSOR_DELAY_GAME);
 	}
 
 	public void disableSensor() {
@@ -45,11 +42,19 @@ public class MainActivity extends Activity implements SensorEventListener {
 
 	@Override
 	public final void onSensorChanged(SensorEvent event) {
-		if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
-			SensorManager.getRotationMatrixFromVector(rotationMatrix , event.values);
-			int xVelocity = -SPEED * Math.round(rotationMatrix[2]*10);
-			mainView.setBallXvelocity(xVelocity);
+		if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			System.arraycopy(event.values, 0, gravityData, 0, 3);
+		} else {
+			System.arraycopy(event.values, 0, magneticData, 0, 3);
 		}
+
+		float[] R={0,0,0,0,0,0,0,0,0};
+
+		if(SensorManager.getRotationMatrix(R, null, gravityData, magneticData)) {
+			SensorManager.getOrientation(R, tiltData);
+		}
+
+		mainView.setBallXvelocity((int) (Math.round(tiltData[2])*10));
 	}
 
 	@Override
